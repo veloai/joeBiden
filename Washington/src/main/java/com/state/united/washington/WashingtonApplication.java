b6d8686d55e9ddca1b609f4c2ca79932e6590f70
wjson <wjson@enliple.com>
2022-05-26 18:25:04 +0900

    h2 properties

---
+++ Washington/src/main/java/com/state/united/washington/WashingtonApplication.java
@@ -0,0 +1,13 @@
+package com.state.united.washington;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashingtonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WashingtonApplication.class, args);
+    }
+
+}
---
+++ Washington/src/test/java/com/state/united/washington/WashingtonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.state.united.washington;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashingtonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
